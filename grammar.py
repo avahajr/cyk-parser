fc07667ffb0fd233c955ff03b4b2655d00e9fb6b
Ava Hajratwala <100488972+avahajr@users.noreply.github.com>
2023-10-12 22:43:29 -0400

    finished p2

--- grammar.py
+++ grammar.py
@@ -8,36 +8,35 @@ import sys
 from collections import defaultdict
 from math import fsum, isclose
 
-class Pcfg(object): 
+
+class Pcfg(object):
     """
-    Represent a probabilistic context free grammar. 
+    Represent a probabilistic context free grammar.
     """
 
-    def __init__(self, grammar_file): 
+    def __init__(self, grammar_file):
         self.rhs_to_rules = defaultdict(list)
         self.lhs_to_rules = defaultdict(list)
-        self.startsymbol = None 
-        self.read_rules(grammar_file)      
- 
-    def read_rules(self,grammar_file):
-        
-        for line in grammar_file: 
+        self.startsymbol = None
+        self.read_rules(grammar_file)
+
+    def read_rules(self, grammar_file):
+        for line in grammar_file:
             line = line.strip()
             if line and not line.startswith("#"):
-                if "->" in line: 
+                if "->" in line:
                     rule = self.parse_rule(line.strip())
                     lhs, rhs, prob = rule
                     self.rhs_to_rules[rhs].append(rule)
                     self.lhs_to_rules[lhs].append(rule)
-                else: 
+                else:
                     startsymbol, prob = line.rsplit(";")
                     self.startsymbol = startsymbol.strip()
-                    
-     
-    def parse_rule(self,rule_s):
+
+    def parse_rule(self, rule_s):
         lhs, other = rule_s.split("->")
         lhs = lhs.strip()
-        rhs_s, prob_s = other.rsplit(";",1) 
+        rhs_s, prob_s = other.rsplit(";", 1)
         prob = float(prob_s)
         rhs = tuple(rhs_s.strip().split())
         return (lhs, rhs, prob)
@@ -45,26 +44,34 @@ class Pcfg(object):
     def verify_grammar(self):
         """
         Return True if the grammar is a valid PCFG in CNF.
-        Otherwise return False. 
+        Otherwise return False.
         """
-        nonterminals = self.lhs_to_rules.keys() # problem spec allows for this assumption
+        nonterminals = self.lhs_to_rules.keys()
+        # problem spec allows for this assumption
         for ruleset in self.lhs_to_rules.values():
-            if not isclose(sum(r[-1] for r in ruleset), 1): return False
+            if not isclose(sum(r[-1] for r in ruleset), 1):
+                return False
             for rule in ruleset:
-                if not (all(sym in nonterminals for sym in rule[1]) or all(sym not in nonterminals for sym in rule[1])) :
+                if not (
+                    all(sym in nonterminals for sym in rule[1])
+                    or all(sym not in nonterminals for sym in rule[1])
+                ):
                     return False
-        return True
+        return "TOP" in nonterminals
 
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
+
 if __name__ == "__main__":
-    with open(sys.argv[1],'r') as grammar_file:
+    with open(sys.argv[1], "r") as grammar_file:
         grammar = Pcfg(grammar_file)
+
         if not grammar.verify_grammar():
-            eprint("** ERR! The grammar in file", sys.argv[1], "is not a valid grammar.")
-        else:
-            print("The grammar is valid. Continuing...")
-            
-        
+            eprint(
+                "** ERR! The grammar in file", sys.argv[1], "is not a valid grammar."
+            )
+            sys.exit(1)
+
+        print("The grammar is valid. Continuing...")
--- unit_tests.py
+++ unit_tests.py
@@ -1,23 +1,38 @@
 from grammar import Pcfg
+from cky import *
+
 
 def test_verify_grammar():
     # Test 1: Valid PCFG Grammar
-    with open('unit_tests/toy_grammar.pcfg', 'r') as grammar_file:
+    with open("test_grammars/toy_grammar.pcfg", "r") as grammar_file:
         grammar = Pcfg(grammar_file)
-    
+
     if grammar.verify_grammar():
         print("Test 1: Valid PCFG Grammar - Passed")
+        [print(str(key) + ":", val) for key, val in grammar.rhs_to_rules.items()]
     else:
         print("Test 1: Valid PCFG Grammar - Failed")
 
     # Test 2: Invalid PCFG Grammar
-    with open('unit_tests/invalid_grammar.pcfg', 'r') as grammar_file:
+    with open("test_grammars/invalid_grammar.pcfg", "r") as grammar_file:
         grammar = Pcfg(grammar_file)
-    
+
     if not grammar.verify_grammar():
         print("Test 2: Invalid PCFG Grammar - Passed")
     else:
         print("Test 2: Invalid PCFG Grammar - Failed")
 
-if __name__ == '__main__':
+
+def test_cky_algo():
+    with open("test_grammars/toy_grammar.pcfg", "r") as grammar_file:
+        grammar = Pcfg(grammar_file)
+        parser = CkyParser(grammar)
+        if parser.is_in_language("she saw the cat with glasses".split()):
+            print("Test 3: Language membership - Passed")
+        else:
+            print("Test 3: Language membership - Failed")
+
+
+if __name__ == "__main__":
     test_verify_grammar()
+    test_cky_algo()
